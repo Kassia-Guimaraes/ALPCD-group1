892c98e58f831b2a454fa305bb333a88fd22bf93
Sara Marques <a85571@alunos.uminho.pt>
2024-10-26 11:29:01 +0100

    Estrutura geral do trabalho

--- jobscli.py
+++ jobscli.py
@@ -1,3 +1,31 @@
-from top_secret import secret 
+from top_secret import secret
+from datasets import import_data, export_csv
 import requests
-import typer 
+import typer
+
+
+app = typer.Typer()
+
+
+@app.command()
+def top():
+    return
+
+
+@app.command()
+def search():
+    return
+
+
+@app.command()
+def salary():
+    return
+
+
+@app.command()
+def skills():
+    return
+
+
+if __name__ == "__main__":
+    app()
