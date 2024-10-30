5b3551e9aaae23e2a25517af119b7d3e8be4e461
Kassia-Guimaraes <kassiavivereduarda@gmail.com>
2024-10-30 14:22:38 +0000

    criação função para ver salário de determinado trabalho

--- jobscli.py
+++ jobscli.py
@@ -1,5 +1,5 @@
 from top_secret import secret
-from datasets import import_data, export_csv
+from datasets import import_data, export_csv, request_data
 import requests
 import typer
 
@@ -18,8 +18,38 @@ def search():
 
 
 @app.command()
-def salary():
-    return
+def salary(job_id: int):
+    try:
+        #total_data = request_data('https://api.itjobs.pt/', path='job/search.json', limit=1, page=1)['total'] # num dados que existem 
+
+        data_list = import_data('https://api.itjobs.pt/', path='job/search.json', limit=1, total_data=5) # lista com todos os resultados da página
+
+        if data_list is None: # se data_list é None, imprime uma mensagem e encerra a função
+            print("Erro: Nenhum dado retornado.")
+            return 'Nenhum dado retornado'
+
+        # Itera sobre cada item da lista de dados
+        for data in data_list:
+
+            # Verifica se 'id' e 'wage' estão no dicionário
+            if ('id' in data) and ('wage' in data) and (data['id'] == job_id):
+                print('data[id]:', data['id'])
+                print('wage:', data['wage'])
+                
+                # Verifica se 'wage' tem valor
+                if data['wage']:
+
+                    print("Wage:", data['wage'])
+
+                return data['wage']
+
+        # Caso não encontre o job_id
+        print('JobID não encontrado.')
+        return 'JobID não encontrado'
+
+    except Exception as e:
+        print(f'Erro: {e}')
+        return e
 
 
 @app.command()
