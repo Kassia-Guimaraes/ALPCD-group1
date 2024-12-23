13cc861819c72df31475f45be5aff54557622e66
Kassia-Guimaraes <kassiavivereduarda@gmail.com>
2024-12-23 17:30:29 +0000

    Ficheiro apenas com função do trabalho prático anterior

--- jobscli.py
+++ jobscli.py
@@ -148,21 +148,8 @@ def dict_csv(data):
     return csv_jobs_info
 
 
-@app.command(help= "Procura o trabalho pelo ID da vaga")
-def fetch_job_details(job_id):
-    jobData = request_data_by_id("https://api.itjobs.pt/", "job/get.json", job_id) 
-    
-    print("Dados do Job:", jobData["company"]["name"])
-    company_name= jobData['company']['name']
-    company_path= company_name.replace(' ', '-')
-    
-    #data= request_html('https://www.ambitionbox.com/overview/', f"{company_path}-overview")
-    data= request_html('https://www.ambitionbox.com/overview/', "capgemini-overview")
-    return data
-
-
 @app.command(help='Encontrar as publicações de emprego mais recentes')
-def top(n: int = typer.Argument('número de vagas'),
+def top(n: int = typer.Argument(help='número de vagas'),
         export: bool = typer.Option(False, "--export", "-e", help="Exportar os resultados para um arquivo CSV")):
 
     # Lista os N trabalhos mais recentes publicados pela itjobs.pt
