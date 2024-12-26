91f1ff0c6672ebf1cb5c7f3daccd386ac70d5c51
Sara Marques <a85571@alunos.uminho.pt>
2024-12-26 18:51:02 +0000

    Lista das skills

---
+++ data.py
@@ -0,0 +1,52 @@
+skills = [
+            # Linguagens de Programação
+            "python", "java", "javascript", "c#", "ruby", "php",
+            "swift", "go", "kotlin", "rust", "typescript", "scala",
+            "perl", "c", "c++", "dart",
+
+            # Desenvolvimento Web
+            "html", "css", "react", "angular", "vue.js", "bootstrap",
+            "node.js", "express", "jquery", "sass", "less",
+
+            # Desenvolvimento de Aplicativos
+            "flutter", "react native", "ionic", "xamarin",
+
+            # Banco de Dados
+            "sql", "nosql", "mongodb", "postgresql", "mysql",
+            "sqlite", "oracledb", "redis", "firebase",
+
+            # DevOps e Infraestrutura
+            "docker", "kubernetes", "aws", "azure",
+            "terraform", "ansible", "jenkins", "git", "ci/cd",
+
+            # Ciência de Dados e Machine Learning
+            "data science", "data analyst", "tensorflow",
+            "pytorch", "pandas", "numpy", "r", "matplotlib",
+            "scikit-learn", "keras", "statistics", "ciência de dados",
+            "estatística", "databases", "bases de dados",
+
+            # Metodologias e Ferramentas
+            "agile", "scrum", "kanban", "devops", "github", "bitbucket",
+            "jira", "trello", "confluence",
+
+            # Segurança da Informação
+            "cybersecurity",
+
+            # Outras Skills Relevantes
+            "blockchain", "iot", "ar/vr", "ui/ux", "seo",
+            "api", "development", "graphql", "performance",
+            "analytics projects", "excel",
+
+            # Soft Skills
+            "communication", "comunicação", "teamwork",
+            "adaptability", "leadership", "trabalho em equipa",
+            "espirito critico", "proatividade", "espirito de equipa",
+            "criatividade",
+
+            # Linguas
+            "inglês", "françes", "espanhol", "português", "english",
+
+            # Licenciaturas
+            "engenharia informática", "ciência de dados", "data engineer",
+            "software engineer", "engenharia de software"
+        ]
